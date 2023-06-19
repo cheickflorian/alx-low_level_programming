894425f3328bacdaaaab8d6437ece662db1b0d4d
cheickflorian <nganjicheick@yahoo.com>
2023-06-19 10:52:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+	for (lower = 'a'; lower <= 'z'; lower++)
+		putchar(lower);
+	putchar('\n');
+	return (0);
+}
