efbe27716bf463c0063b758d6f6a54b676e69d6c
cheickflorian <nganjicheick@yahoo.com>
2023-07-28 06:00:39 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,25 @@
 #include "lists.h"
-
 /**
  * print_list - print all the elements of list_t
  * @h: parameter list
  * Return: structure
  */
-
-size_t print_list(const list_t *h) 
+size_t print_list(const list_t *h)
 {
-    size_t count;
-    count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL) {
-            printf("[%d] %s\n", 0, "(nil)");
-        } else {
-            printf("[%d] %s\n", h->len, h->str);;
-        }
-
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
-
