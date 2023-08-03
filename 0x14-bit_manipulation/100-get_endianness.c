f59f070803f26fa26e00c98b1da44663f6d8634e
cheickflorian <nganjicheick@yahoo.com>
2023-08-03 14:01:15 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
  * get_endianness -  Checks the endianness
- *
  * Return: 
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	char *ptr =  (char *)&num;
-	return (int)(*ptr);
+	char *ptr = (char *)&num;
+	return ((int)(*ptr));
 }
